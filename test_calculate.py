62d757f7f00410d561c16277f7691a40396182b7
Student-02-Mike <66924226+Student-02-Mike@users.noreply.github.com>
2020-06-14 23:17:54 +0100

    Update test_calculate.py

--- test_calculate.py
+++ test_calculate.py
@@ -4,4 +4,8 @@ import calculate
 
 class Calculate(unittest.TestCase):
     # TODO
+    def test_add(self):
+        self.assertEqual(calculate.add(5, 10), 15)
 
+    def test_subtract(self):
+        self.assertEqual(calculate.subtract(10, 5), 5)
